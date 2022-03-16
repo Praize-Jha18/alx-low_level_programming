f2187c1f851a78e8df38d0c9e438ce9a80d34b8b
Praize-Jha18 <praisejahfrancis@gmail.com>
2022-03-15 22:01:00 -0700

    My commit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * print_times_tables - function that prints the n times table, starting with 0.
+ * print_times_table - function that prints the n times table, starting with 0.
  * @n: input of table n
  * Return: nothing.
  */
 
-void print_times_tables(int n)
+void print_times_table(int n)
 {
 int row, col;
 int mult = 0;
