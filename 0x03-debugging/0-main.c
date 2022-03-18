f8384405114f662f1beecd96ccd08cb90f440db4
Praize-Jha18 <praisejahfrancis@gmail.com>
2022-03-17 21:53:04 -0700

    My commit

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,16 +2,15 @@
 
 /**
  * main - Debugging.
+ *
  * Return: 0
  */
 
 int main(void)
 {
-int i;
+	int i;
 
-i = 0;
-positive_or_negative(i);
-
-return (0);
+	i = 0;
+	positive_or_negative(i);
+	return (0);
 }
-
