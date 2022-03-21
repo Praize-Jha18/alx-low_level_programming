750e67fba9bc70164bcd5fec47cd3aeb80d57689
Praize-Jha18 <praisejahfrancis@gmail.com>
2022-03-21 16:11:57 -0700

    My commit

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -13,9 +13,9 @@ int i, d, n, len, f, digit;
 i = 0;
 d = 0;
 n = 0;
-len - 0;
-f - 0;
-digit - 0;
+len = 0;
+f = 0;
+digit = 0;
 
 while (s[len] != '\0')
 len++;
@@ -24,7 +24,6 @@ while (i < len && f == 0)
 {
 if (s[i] == '-')
 ++d;
-
 if (s[i] >= '0' && s[i] <= '9')
 {
 digit = s[i] - '0';
